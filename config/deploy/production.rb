ce35ed5b563a3f8508abd1301a76ab56b85004a2
aescend <aescend@tutamail.com>
2020-02-29 21:28:23 -0500

    Changing some file names -- should probably be working with this machine now, if I can figure out ruby?

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,10 @@
-set :user, "danbooru"
+set :user, "aescend"
 set :rails_env, "production"
 set :delayed_job_workers, 12
 append :linked_files, ".env.production"
 
-server "kagamihara", :roles => %w(web app cron), :primary => true
+server "cygnus", :roles => %w(web app cron), :primary => true
+server "kagamihara", :roles => %w(web app cron)
 server "shima", :roles => %w(web app)
 server "saitou", :roles => %w(web app)
 server "oogaki", :roles => %w(worker)
